be049ce83ef3816ffc12dd02b8bdfe83d7983594
gangadhar195 <gangadhars9545@gmail.com>
2025-01-24 00:06:30 +0530

    Add Reduce Function

--- src/ReactHooks/ReactReduce.jsx
+++ src/ReactHooks/ReactReduce.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 const ReactReduce = () => {
     let arr = [1,2,3,4,5];
     const result = arr.reduce((accum,curre)=>accum*curre)
+    const mul = arr.map((item)=>item*2);
 
   return (
     <div>
+        <h5>Array Element</h5>
+      <p>{
+        arr.map((item)=>item + "  ")
+        }</p>
       <h5>Array Multiplication Using Reduce Function</h5>
       <p>{
         result
+        }</p>
+        <h5>Array Multiply each item Using Map Function</h5>
+      <p>{
+        mul.map((item)=>item + "  ")
         }</p>
     </div>
   )
